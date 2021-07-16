ad9e7d15fe261be908ad45a2a7f16c9b84b3e24b
hankisun <61639250+hankisun@users.noreply.github.com>
2021-07-16 17:37:12 +0900

    Update anynode_test.py

--- anynode_test.py
+++ anynode_test.py
@@ -25,5 +25,79 @@ while q:
 q.clear()
 
 print(RenderTree(root))
+
+##########################
+# fusion type
+def cut_range(tr, cur, typ, start_idx):
+    ret = []
+    tr_s = tr[0]
+    tr_e = tr[1]
+    for idx in range(start_idx, len(cur)):
+        ctyp = typ[idx]
+        if ctyp == 'd':
+            continue
+        cr = cur[idx]
+        cr_s = cr[0]
+        cr_e = cr[1]
+
+        if tr_s > cr_e:
+            continue
+
+        if tr_e < cr_s:
+            break
+                        
+        if tr_s >= cr_s:
+            # tr  --- | ----
+            # cr ---  | ---          
+            if tr_e > cr_e:
+                if ctyp == 'c':
+                    ret.append([tr_s, cr_e])
+                tr_s = cr_e + 1
+                continue
+            # tr  --  | --- | ---  |  ---
+            # cr ---- | --- | ---- | ----              
+            else:
+                if ctyp == 'c':
+                    ret.append([cr_s, cr_e])
+                break
     
+        # tr ---  | ----
+        # cr  --- |  ---      
+        if tr_s < cr_s:
+            ret.append([tr_s, cr_s-1])
+            tr_s = cr_s
+            # tr --- 
+            # cr --  
+            if tr_e > cr_e:
+                if ctyp == 'c':
+                    ret.append([tr_s, cr_e])
+                tr_s = cr_e + 1
+                continue
+            # tr --  | --- 
+            # cr --- | ---
+            else:
+                if ctyp == 'c':
+                    ret.append([tr_s, cr_e])
+                break
+
+    print(ret)
+    return ret, idx if idx > 0 else idx -1
+
+trace_line = [[4,11],[15,19]]
+cur = [[1,2],[3,6],[8,9],[11,15],[17,17],[19,20]]
+typ = ['c','a','c','a','d','c']
+# cur = [[2,21]]
+
+cut_list = []
+next_idx = 0
+for tr in trace_line:
+    cut, next_idx = cut_range(tr, cur, typ, next_idx)
+    if cut_list:
+        if cut_list[-1] == cut[0]:
+            cut.pop(0)
+        cut_list.extend(cut)
+    else:
+        cut_list = cut
+
+print(cut_list)
     
