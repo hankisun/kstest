dade199178aed318b1ee18a83715b3dccaf33726
hankisun <61639250+hankisun@users.noreply.github.com>
2021-07-19 15:30:01 +0900

    Update anynode_test.py

--- anynode_test.py
+++ anynode_test.py
@@ -28,88 +28,107 @@ print(RenderTree(root))
 
 ##########################
 # fusion type
-class CutRange:
-    def __init__(self, cur_line, typ):
-        self.cur_line = cur_line
-        self.typ = typ
-        self.tr_idx = 0
+# fusion type
+def cut_range(tr, cur, typ, start_idx):
+    ret = []
+    ret_idx = []
+    tr_s = tr[0]
+    tr_e = tr[1]
+    cut_flag = False
+    for idx in range(start_idx, len(cur)):
+        ctyp = typ[idx]
+        cr = cur[idx]
+        cr_s = cr[0]
+        cr_e = cr[1]
+
+        if tr_s > cr_e:
+            continue
+
+        if tr_e < cr_s:
+            ret.append([tr_s, tr_e])
+            ret_idx.append(-1)
+            break
+            
+        if ctyp == 'd':  # cr_s, cr_e has the same value
+            if tr_s <= cr_s and tr_e >= cr_s:
+                ret.append([tr_s, cr_s])
+                ret_idx.append(-1)
+                tr_s = cr_s + 1
+            continue
+                        
+        if tr_s >= cr_s:
+            # tr  --- | ----
+            # cr ---  | ---          
+            if tr_e > cr_e:
+                if ctyp == 'c':
+                    ret.append([tr_s, cr_e])
+                    ret_idx.append(idx)
+                tr_s = cr_e + 1
+                continue
+            # tr  --  | --- | ---  |  ---
+            # cr ---- | --- | ---- | ----              
+            else:
+                if ctyp == 'c':
+                    ret.append([cr_s, cr_e])
+                    ret_idx.append(idx)
+                break
     
-    def cut_range(self, tr):
-        ret = []
-        tr_s = tr[0]
-        tr_e = tr[1]
-        for idx in range(self.tr_idx, len(self.cur_line)):
-            ctyp = self.typ[idx]
-            if ctyp == 'd':
+        # tr ---  | ----
+        # cr  --- |  ---      
+        if tr_s < cr_s:
+            ret.append([tr_s, cr_s-1])
+            ret_idx.append(-1)
+            tr_s = cr_s
+            # tr --- 
+            # cr --  
+            if tr_e > cr_e:
+                if ctyp == 'c':
+                    ret.append([tr_s, cr_e])
+                    ret_idx.append(idx)
+                tr_s = cr_e + 1
                 continue
-            cr = self.cur_line[idx]
-            cr_s = cr[0]
-            cr_e = cr[1]
-
-            if tr_s > cr_e:
-                continue
-
-            if tr_e < cr_s:
+            # tr --  | --- 
+            # cr --- | ---
+            else:
+                if ctyp == 'c':
+                    ret.append([tr_s, cr_e])
+                    ret_idx.append(idx)
                 break
 
-            if tr_s >= cr_s:
-                # tr  --- | ----
-                # cr ---  | ---          
-                if tr_e > cr_e:
-                    if ctyp == 'c':
-                        ret.append([tr_s, cr_e])
-                    tr_s = cr_e + 1
-                    continue
-                # tr  --  | --- | ---  |  ---
-                # cr ---- | --- | ---- | ----              
-                else:
-                    if ctyp == 'c':
-                        ret.append([cr_s, cr_e])
-                    break
+    if tr_s <= tr_e and tr_s > cur[idx][1]:
+        ret.append([tr_s, tr_e])
+        ret_idx.append(-1)
 
-            # tr ---  | ----
-            # cr  --- |  ---      
-            if tr_s < cr_s:
-                ret.append([tr_s, cr_s-1])
-                tr_s = cr_s
-                # tr --- 
-                # cr --  
-                if tr_e > cr_e:
-                    if ctyp == 'c':
-                        ret.append([tr_s, cr_e])
-                    tr_s = cr_e + 1
-                    continue
-                # tr --  | --- 
-                # cr --- | ---
-                else:
-                    if ctyp == 'c':
-                        ret.append([tr_s, cr_e])
-                    break
+    print(ret)
+    return ret, ret_idx, idx-1 if idx > 0 else 0
 
-        print(ret)
-        
-        self.tr_idx = idx if idx > 0 else idx -1
-        return ret
-
-trace_line = [[4,11],[15,19]]
-cur = [[1,2],[3,6],[8,9],[11,15],[17,17],[19,20]]
-typ = ['c','a','c','a','d','c']
+trace_line = [[6,10],[15,20]]
+cur = [[1,2],[3,6],[8,9],[11,15],[17,17],[20,20]]
+typ = ['c','a','c','a','d','d']
 # cur = [[2,21]]
 
 cut_list = []
+cut_idx_list = []
 next_idx = 0
 
-cr = CutRange(cur, typ)
+# if trace_line[-1][1] > cur[-1][1]:
+#     dummy_last = trace_line[-1][1] + 1 
+#     cur.append([dummy_last, dummy_last])
+
 for tr in trace_line:
-    cut = cr.cut_range(tr)
+    cut, cut_idx, next_idx = cut_range(tr, cur, typ, next_idx)
     if cut_list:
         if cut_list[-1] == cut[0]:
             cut.pop(0)
+            cut_idx.pop(0)
         cut_list.extend(cut)
+        cut_idx_list.extend(cut_idx)
     else:
         cut_list = cut
+        cut_idx_list = cut_idx
 
 print(cut_list)
+print(cut_idx_list)
 
 ##########################
 # fusion type
